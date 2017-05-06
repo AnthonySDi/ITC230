1368520b20fe2d18b10697cf968934be85389b65
Anthony <stenbergdigeronimo@gmail.com>
2017-05-06 15:43:32 -0700

    Assignment3
    Still have a bug to work out.

---
+++ assign3Git/routes/index.js
@@ -0,0 +1,16 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.home = function (req, res) {
+    res.render('index', { title: 'Sci-fi Books', year: new Date().getFullYear() });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+};
+
+exports.details = function (req, res) {
+    res.render('Details', { title: 'Details', year: new Date().getFullYear(), message: 'Your contact page' });
+};
