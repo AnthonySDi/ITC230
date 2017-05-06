2eba88b8b043235cf20c0c5c5eca43ffba22826d
Anthony <stenbergdigeronimo@gmail.com>
2017-05-06 15:44:29 -0700

    Assignment2
    This is now a functional

--- Assignment2/index.js
+++ Assignment2/index.js
@@ -1,6 +1,12 @@
 // JavaScript source code
 var http = require('http'),
-	fs = require('fs');
+	fs = require('fs'),
+	qs = require("querystring");
+
+
+
+let scifibooks = require('./lib/books.js');
+
 
 function serveStaticFile(res, path, contentType, responseCode) {
     if (!responseCode) responseCode = 200;
@@ -21,12 +27,33 @@ http.createServer(function (req, res) {
     // trailing slash, and making lowercase
     var path = req.url.replace(/\/?(?:\?.*)?$/, '')
 		.toLowerCase();
+    var url = req.url.split("?"),
+        params = qs.parse(url[1]);
     switch (path) {
         case '':
             serveStaticFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
             serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/search':
+            serveStaticFile(res, '/public/search', 'text/html');
+            break;
+        //case '/add':
+        //    serveStaticFile(res, '/public/add', 'text/html');
+            //    break;
+        case '/get':
+                
+            let found = scifibooks.get(params.title);
+           
+            res.writeHead(200, { 'content-type': 'text/plain' });
+            let results = (found) ? JSON.stringify(found) : "Not found";
+            res.end('results for ' + params.title + "..............." + results);
+            break;
+        case '/delete':
+            res.writeHead(200, { 'content-Type': 'text/plain' });
+            res.end('delete');
+            //serveStaticFile(res, '/public/delete', 'text/html');
             break;
         case '/img/logo.jpg':
             serveStaticFile(res, '/public/img/logo.jpg',
